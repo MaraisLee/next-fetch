7ce5f94513cee5020095fdbb67034a340db6eece
Yenhj <yen.greact@gmail.com>
2023-03-21 10:34:56 +0900

    Next.js 생성

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+// 전체 화면 레이아웃 설정
+// css 및 플러그인들을 설정
+// 공통으로 적용될 컴포넌트(레이아웃) 설정
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
