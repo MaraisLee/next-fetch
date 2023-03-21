501daa2701bdcee5e15b76793a56b94c4e43347a
Yenhj <yen.greact@gmail.com>
2023-03-21 12:38:37 +0900

    기본레이아웃 잡는중 '

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>404 엄슴</div>;
+  return <div>404 임당</div>;
 };
 
 export default NotFoundPage;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+import Layout from "../components/Layout";
 import "../styles/globals.css";
-// 전체 화면 레이아웃 설정
-// css 및 플러그인들을 설정
-// 공통으로 적용될 컴포넌트(레이아웃) 설정
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
 export default MyApp;
