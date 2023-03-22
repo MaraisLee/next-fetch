4eb08a1c5371ab72b9279bdca8f2404560d4f18e
Yenhj <yen.greact@gmail.com>
2023-03-22 11:36:35 +0900

    SSG 적용

--- pages/index.js
+++ pages/index.js
@@ -19,18 +19,36 @@ export default function Home({ cats }) {
 // CSR : Client-Side-Rendering
 // SSR : Server-Side-Rendering ( 요청시 HTML 생성 후 브라우저 출력)
 // SSG : Static-Site-Generation ( 서버에서 미리 HTML 생성, 변경불가)
+
 // 1. SSR 적용
 // 데이터 호출 후 Response 된 결과를 Props로 전달한다. (자동으로)
-// 서버에서 데이터 바꾸고 다시 돌리면 client도 변경됨.
-export const getServerSideProps = async () => {
-  // const res = await fetch(
-  //   "https://api.thecatapi.com/v1/breeds?api_key=live_Z9dx0VtiK2f0qbMsh1fhE7Z3Sw21vaP79MAhtKChl3XFPpWKvoBDSa6OSqZHYNSJ&limit=10"
-  // );
+// 서버에서 데이터 바꾸고 다시 돌리면 client도 변경됨. (새로고침 시)
+// export const getServerSideProps = async () => {
+//   // const res = await fetch(
+//   //   "https://api.thecatapi.com/v1/breeds?api_key=live_Z9dx0VtiK2f0qbMsh1fhE7Z3Sw21vaP79MAhtKChl3XFPpWKvoBDSa6OSqZHYNSJ&limit=10"
+//   // );
+//   const res = await fetch("http://localhost:8080/api/cats");
+//   const cats = await res.json();
+//   return {
+//     props: {
+//       cats,
+//     },
+//   };
+// };
+
+// 2. SSG 적용 (Static-Site-Generation)
+//    npm run build 할때 html을 미리 만들어서 포함한다.
+//    새로고침을 해도 내용 갱신이 안된다.
+//    but, 미리 만들어져 있어서 엄청 빨리 화면에 출력
+//    so, Next.js 에서는 SSG 적극 추천 why? SEO 도 잘됨
+//    getStaticProps 지원
+export const getStaticProps = async () => {
   const res = await fetch("http://localhost:8080/api/cats");
   const cats = await res.json();
   return {
     props: {
       cats,
     },
+    revalidate: 20,
   };
 };
